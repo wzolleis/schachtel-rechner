f9021a0f78f552e49338b1e27773f3b9c9934ef6
Jack <wzolleis@web.de>
2025-08-21 23:52:27 +0200

    Dashboard-Seite

--- src/features/landingpage/landing-page.tsx
+++ src/features/landingpage/landing-page.tsx
@@ -1,9 +1,10 @@
 import {BoxCalcForm} from "@/features/box-calc-gehrung/box-calc-form";
+import Page from "@/dashboard/page";
 
 export const LandingPage = () => {
     return (
         <>
-            Landing page
+            <Page/>
             <BoxCalcForm/>
         </>
     )
